803c846486aeb9286cde8a5f0866bb03ac7eb5ba
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-11-28 15:12:55 +0530

    Removed extra spaces

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const { app, BrowserWindow } = require('electron')
 // include the Node.js 'path' module at the top of your file
 const path = require('path')
 
-
-
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
